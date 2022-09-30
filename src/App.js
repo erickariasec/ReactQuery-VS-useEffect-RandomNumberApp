224dd1700a655c59e2019e56f4b7be2c13b2c572
erickariasec <erickarias1999@hotmail.com>
2022-09-30 09:40:09 -0500

    Adding App structure, styling and fuctions to get Random Numbers using useEffect and React Query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import { useQuery } from '@tanstack/react-query';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetchNumber = () => {
+    return fetch(
+        "https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new"
+    ).then((response) => {
+        if (response.status !== 200) {
+            throw new Error(`Something went wrong. Try again.`);
+        }
+
+        return response.text();
+    });
+};
+
+const ReactQueryRandom = () => {
+    const query = useQuery(["random"], fetchNumber, {
+        refetchOnWindowFocus: false,
+    });
+
+    if (query.isError) return <p>{query.error.message}</p>;
+
+    return (
+        <button onClick={() => query.refetch()}>
+            Random number:{" "}
+            {query.isLoading || query.isFetching ? "..." : query.data}
+        </button>
+    );
+};
+
+const UseEffectRandom = () => {
+    const [key, forceUpdate] = React.useReducer((x) => x + 1, 0);
+    const [num, setNum] = React.useState();
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState("");
+
+    React.useEffect(() => {
+        setLoading(true);
+
+        fetch(
+            "https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new"
+        )
+            .then((response) => {
+                if (response.status !== 200) {
+                    return {
+                        error: `Something went wrong. Try again.`,
+                    };
+                }
+
+                return response.text();
+            })
+            .then((random) => {
+                setLoading(false);
+
+                if (isNaN(Number(random))) {
+                    const errorResponse = JSON.parse(random);
+                    setError(errorResponse.error);
+                } else {
+                    setNum(random);
+                }
+            });
+    }, [key]);
+
+    if (error) return <p>{error}</p>;
+
+    return (
+        <button onClick={forceUpdate}>
+            Random number: {loading ? "..." : num}
+        </button>
+    );
+};
+
+export default function App() {
+    return (
+        <div>
+            <h1>Get Random Numbers</h1>
+            <h2>useEffect</h2>
+            <UseEffectRandom />
+            <h2>React Query</h2>
+            <ReactQueryRandom />
+        </div>
+    );
 }
-
-export default App;
